22b5797a8bd2623718ae95f59806418ae6a7ea72
sayalishelke <sayalishelke@microsoft.com>
2023-07-25 23:50:37 +0530

    start server code

--- app.js
+++ app.js
@@ -91,3 +91,10 @@ app.post('/api/post/:postId/comment', async (req, res) => {
     await post.save();
     res.json(post);
   });
+
+
+// Finally Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
